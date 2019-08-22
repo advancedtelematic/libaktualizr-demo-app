6ea6ba1d3708481aaf9565594b58a2cc18c86f73
Eugene Smirnov <evgenii.smirnov@here.com>
2019-08-22 16:59:49 +0200

    Application code cleanup
    Signed-off-by: Eugene Smirnov <evgenii.smirnov@here.com>

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <map>
-#include <mutex>
 #include <string>
 #include <vector>
 
-#include <openssl/ssl.h>
 #include <boost/algorithm/string.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
-#include <boost/property_tree/ini_parser.hpp>
 #include <boost/signals2.hpp>
 
 #include "config/config.h"
@@ -18,67 +15,28 @@
 
 namespace bpo = boost::program_options;
 
-void check_info_options(const bpo::options_description &description, const bpo::variables_map &vm) {
-  if (vm.count("help") != 0) {
-    std::cout << description << '\n';
-    std::cout << "Available commands: Shutdown, SendDeviceData, CheckUpdates, Download, Install, CampaignCheck\n";
-    exit(EXIT_SUCCESS);
-  }
-  if (vm.count("version") != 0) {
-    std::cout << "Current hmi_stub version is: " << aktualizr_version() << "\n";
-    exit(EXIT_SUCCESS);
-  }
-}
-
 bpo::variables_map parse_options(int argc, char *argv[]) {
-  bpo::options_description description("HMI stub interface for libaktualizr");
-  // clang-format off
-  // Try to keep these options in the same order as Config::updateFromCommandLine().
-  // The first three are commandline only.
+  bpo::options_description description("A simple wrapper for libaktualizr");
   description.add_options()
-      ("help,h", "print usage")
-      ("version,v", "Current aktualizr version")
       ("config,c", bpo::value<std::vector<boost::filesystem::path> >()->composing(), "configuration file or directory")
+      ("help,h", "print help message")
       ("secondary-configs-dir", bpo::value<boost::filesystem::path>(), "directory containing seconday ECU configuration files")
       ("loglevel", bpo::value<int>(), "set log level 0-5 (trace, debug, info, warning, error, fatal)");
-  // clang-format on
 
   bpo::variables_map vm;
   std::vector<std::string> unregistered_options;
-  try {
-    bpo::basic_parsed_options<char> parsed_options =
-        bpo::command_line_parser(argc, argv).options(description).allow_unregistered().run();
-    bpo::store(parsed_options, vm);
-    check_info_options(description, vm);
-    bpo::notify(vm);
-    unregistered_options = bpo::collect_unrecognized(parsed_options.options, bpo::include_positional);
-    if (vm.count("help") == 0 && !unregistered_options.empty()) {
-      std::cout << description << "\n";
-      exit(EXIT_FAILURE);
-    }
-  } catch (const bpo::required_option &ex) {
-    // print the error and append the default commandline option description
-    std::cout << ex.what() << std::endl << description;
-    exit(EXIT_FAILURE);
-  } catch (const bpo::error &ex) {
-    check_info_options(description, vm);
+  bpo::basic_parsed_options<char> parsed_options =
+    bpo::command_line_parser(argc, argv).options(description).allow_unregistered().run();
+  bpo::store(parsed_options, vm);
+  bpo::notify(vm);
+  unregistered_options = bpo::collect_unrecognized(parsed_options.options, bpo::include_positional);
 
-    // log boost error
-    LOG_ERROR << "boost command line option error: " << ex.what();
-
-    // print the error message to the standard output too, as the user provided
-    // a non-supported commandline option
-    std::cout << ex.what() << '\n';
-
-    // set the returnValue, thereby ctest will recognize
-    // that something went wrong
+  if (vm.count("help") != 0 || !unregistered_options.empty()) {
+    std::cout << description << "\n";
     exit(EXIT_FAILURE);
   }
-
   return vm;
 }
-
-static std::vector<Uptane::Target> current_updates;
 
 void process_event(const std::shared_ptr<event::BaseEvent> &event) {
   static std::map<std::string, unsigned int> progress;
@@ -109,8 +67,7 @@ void process_event(const std::shared_ptr<event::BaseEvent> &event) {
               << (install_complete->success ? "success" : "failure") << "\n";
   } else if (event->variant == "UpdateCheckComplete") {
     const auto check_complete = dynamic_cast<event::UpdateCheckComplete *>(event.get());
-    current_updates = check_complete->result.updates;
-    std::cout << current_updates.size() << " updates available\n";
+    std::cout << check_complete->result.updates.size() << " updates available\n";
   } else {
     std::cout << "Received " << event->variant << " event\n";
   }
@@ -121,54 +78,53 @@ int main(int argc, char *argv[]) {
   logger_set_threshold(boost::log::trivial::info);
   LOG_INFO << "demo-app starting";
 
-  bpo::variables_map commandline_map = parse_options(argc, argv);
-
-  int r = EXIT_FAILURE;
-  boost::signals2::connection conn;
-
   try {
+    bpo::variables_map commandline_map = parse_options(argc, argv);
     Config config(commandline_map);
-    if (config.logger.loglevel <= boost::log::trivial::debug) {
-      SSL_load_error_strings();
-    }
-    LOG_DEBUG << "Current directory: " << boost::filesystem::current_path().string();
 
     Aktualizr aktualizr(config);
-    std::function<void(const std::shared_ptr<event::BaseEvent> event)> f_cb =
-        [](const std::shared_ptr<event::BaseEvent> event) { process_event(event); };
-    conn = aktualizr.SetSignalHandler(f_cb);
+
+    auto f_cb = [](const std::shared_ptr<event::BaseEvent> event) { process_event(event); };
+    boost::signals2::scoped_connection conn(aktualizr.SetSignalHandler(f_cb));
 
     aktualizr.Initialize();
 
+    std::vector<Uptane::Target> current_updates;
     std::string buffer;
+    LOG_INFO << "Available commands: SendDeviceData, CheckUpdates, Download, Install, CampaignCheck, CampaignAccept, Pause, Resume, Abort" << std::endl;
     while (std::getline(std::cin, buffer)) {
-      boost::algorithm::to_lower(buffer);
-      if (buffer == "senddevicedata") {
-        aktualizr.SendDeviceData();
-      } else if (buffer == "fetchmetadata" || buffer == "fetchmeta" || buffer == "checkupdates" || buffer == "check") {
-        auto fut_result = aktualizr.CheckUpdates();
-      } else if (buffer == "download" || buffer == "startdownload") {
-        aktualizr.Download(current_updates);
-      } else if (buffer == "install" || buffer == "uptaneinstall") {
-        aktualizr.Install(current_updates);
+      std::vector<std::string> words;
+      boost::algorithm::split(words, buffer, boost::is_any_of("\t "), boost::token_compress_on);
+      std::string &command = words[0];
+      std::string &param = words[1];
+      boost::algorithm::to_lower(command);
+      if (command == "senddevicedata") {
+        aktualizr.SendDeviceData().get();
+      } else if (command == "checkupdates") {
+        auto result = aktualizr.CheckUpdates().get();
+        current_updates = result.updates;
+      } else if (command == "download") {
+        aktualizr.Download(current_updates).get();
+      } else if (command == "install") {
+        aktualizr.Install(current_updates).get();
         current_updates.clear();
-      } else if (buffer == "campaigncheck") {
-        aktualizr.CampaignCheck();
-      } else if (buffer == "pause") {
+      } else if (command == "campaigncheck") {
+        aktualizr.CampaignCheck().get();
+      } else if (command == "campaignaccept") {
+        aktualizr.CampaignControl(param, campaign::Cmd::Accept);
+      } else if (command == "pause") {
         aktualizr.Pause();
-      } else if (buffer == "resume") {
+      } else if (command == "resume") {
         aktualizr.Resume();
-      } else if (buffer == "abort") {
+      } else if (command == "abort") {
         aktualizr.Abort();
-      } else if (!buffer.empty()) {
+      } else if (!command.empty()) {
         std::cout << "Unknown command.\n";
       }
     }
-    r = EXIT_SUCCESS;
+    return EXIT_SUCCESS;
   } catch (const std::exception &ex) {
     LOG_ERROR << "Fatal error in demo-app: " << ex.what();
+    return EXIT_FAILURE;
   }
-
-  conn.disconnect();
-  return r;
 }
